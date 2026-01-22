e3c44e96e9723b60856a18bb799ae3a89cd3cb19
yauhen_bialetski <yauhen_bialetski@epam.com>
2026-01-22 18:14:42 +0100

    New console logs

--- newFeature.js
+++ newFeature.js
@@ -1,1 +1,6 @@
 console.log("New feature module loaded successfully.");
+
+confirm("Do you want to enable the new feature?");
+console.log("New feature enabled.");
+
+console.log("New feature setup completed.");
