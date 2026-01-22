ec54459f663dd8ab5437a1175260f7ea05089ddb
yauhen_bialetski <yauhen_bialetski@epam.com>
2026-01-22 18:29:16 +0100

    added new console to task file from main branch

--- task.js
+++ task.js
@@ -7,3 +7,7 @@ console.log("Task execution completed.");
 console.log("Task is here!");
 
 console.log("Task is not completed!");
+
+console.log("Task module setup finished.");
+console.log("All tasks are done.");
+console.log("No pending tasks.");
